0a8cc432c6e7bc91aefc6f72737d4d4fe57b7eed
He Quanjie Boey <hqjb91@gmail.com>
2021-03-09 14:57:42 +0800

    First Commit

---
+++ src/main/java/com/reku/coolingloadestimation/CoolingLoadEstimationApplication.java
@@ -0,0 +1,13 @@
+package com.reku.coolingloadestimation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolingLoadEstimationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolingLoadEstimationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reku/coolingloadestimation/CoolingLoadEstimationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reku.coolingloadestimation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolingLoadEstimationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
